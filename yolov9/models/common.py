1a26b995f751c3091afaaa6564a674ebd8e7528d
Daheer <suhayrid6@gmail.com>
2024-03-07 11:20:19 +0100

    (Test) added support for backup link to model checkpoint

--- yolov9/models/common.py
+++ yolov9/models/common.py
@@ -679,10 +679,10 @@ class DetectMultiBackend(nn.Module):
         stride = 32  # default stride
         cuda = torch.cuda.is_available() and device.type != 'cpu'  # use CUDA
         if not (pt or triton):
-            w = attempt_download(w, backup=backup)  # download if not local
+            w = attempt_download(w)  # download if not local
 
         if pt:  # PyTorch
-            model = attempt_load(weights if isinstance(weights, list) else w, device=device, inplace=True, fuse=fuse)
+            model = attempt_load(weights if isinstance(weights, list) else w, backup=backup, device=device, inplace=True, fuse=fuse)
             stride = max(int(model.stride.max()), 32)  # model stride
             names = model.module.names if hasattr(model, 'module') else model.names  # get class names
             model.half() if fp16 else model.float()
