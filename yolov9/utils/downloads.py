3a15c61ed39e595d803677abe6ad375cfa060c22
Daheer <suhayrid6@gmail.com>
2024-03-07 11:13:26 +0100

    (Test) added support for backup link to model checkpoint

--- yolov9/utils/downloads.py
+++ yolov9/utils/downloads.py
@@ -45,7 +45,7 @@ def safe_download(file, url, url2=None, min_bytes=1E0, error_msg=''):
         if file.exists():
             file.unlink()  # remove partial downloads
         LOGGER.info(f'ERROR: {e}\nRe-attempting {url2 or url} to {file}...')
-        os.system(f"curl -# -L '{url2 or url}' -o '{file}' --retry 3 -C -")  # curl download, retry and resume on fail
+        os.system(f"curl -# -L '{url2}' -o '{file}' --retry 3 -C -")  # curl download, retry and resume on fail
     finally:
         if not file.exists() or file.stat().st_size < min_bytes:  # check
             if file.exists():
