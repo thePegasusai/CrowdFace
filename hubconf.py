c5a190af9d3e7fca04b97e905d906af0c5a069d2
Daheer <suhayrid6@gmail.com>
2024-03-07 11:08:09 +0100

    (Test) added support for backup link to model checkpoint

--- hubconf.py
+++ hubconf.py
@@ -8,7 +8,6 @@ Usage:
 dependencies = [
     "torch",
     "torchvision",
-    # "ultralytics"
 ]
 
 from yolov9.models.common import AutoShape as _AutoShape
@@ -25,7 +24,7 @@ def detector(auto_shape=True):
     CrowdFace detecting YOLOv9 model
   """
 
-  url = "https://github.com/Ultrabrain/CrowdFace/releases/download/yolov9FaceRecognition/best.pt" 
+  url = "https://github.com/Ultrabraindasd/CrowdFace/releases/download/yolov9FaceRecognition/best.pt" 
   backup = "https://crowdfacefaceetector.s3.amazonaws.com/best.pt"
   model = _DetectMultiBackend(weights=url, backup=backup)
   if auto_shape: model = _AutoShape(model)
